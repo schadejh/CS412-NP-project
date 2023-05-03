e18399c24d62a68756307ff0788020792c0d3282
rzanetti200 <72398310+rzanetti200@users.noreply.github.com>
2023-05-02 21:48:31 -0400

    Exact solution to the maxClique
    The method “find_cliques” employs a backtracking solution to find all cliques within the graph(go into more detail with specific sub-methods)
    
    The method max_clique loops through the set of cliques returned my the find_clique method and returns the first clique with the highest node count.

---
+++ cs412_maxClique_exact.py
@@ -0,0 +1,29 @@
+def find_cliques(graph):
+    def expand(clique, candidates):
+        for node in candidates:
+            if all(node in graph[nbr] for nbr in clique):
+                yield clique + [node]
+
+    def search(cliques, candidates):
+        if not candidates:
+            return cliques
+        for clique in expand(cliques[-1], candidates):
+            cliques = search(cliques + [clique], [n for n in candidates if n in graph[clique[-1]]])
+        return cliques
+
+    return search([[]], list(graph.keys()))
+
+def max_clique(graph):
+    cliques = find_cliques(graph)
+    return max(cliques, key=len)
+
+if __name__ == '__main__':
+    m = int(input().strip())
+    edges = [tuple(input().strip().split()) for _ in range(m)]
+    vertices = set(v for edge in edges for v in edge)
+    graph = {v: set() for v in vertices}
+    for u, v in edges:
+        graph[u].add(v)
+        graph[v].add(u)
+    maxclique = max_clique(graph)
+    print(maxclique)
